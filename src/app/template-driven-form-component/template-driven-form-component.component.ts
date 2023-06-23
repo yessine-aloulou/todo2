1e7b4fa61ac6e256a5febb01eb91fda68625cf60
yessine.aloulou <yessine.aloulou@neoxam.com>
2023-06-23 17:57:24 +0100

    first commit

---
+++ src/app/template-driven-form-component/template-driven-form-component.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-template-driven-form-component',
+  templateUrl: './template-driven-form-component.component.html',
+  styleUrls: ['./template-driven-form-component.component.css']
+})
+export class TemplateDrivenFormComponentComponent {
+done():void{
+  alert("bravo");
+}
+}
